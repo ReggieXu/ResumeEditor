e84a21b4c67f824c090e68c482fd7f9d5a2ff9fa
renguixu <xurengui5261@gmail.com>
2017-08-23 17:16:44 -0700

    Initial commit

---
+++ app/src/main/java/edu/udayton/resumeeditor/util/DateUtils.java
@@ -0,0 +1,29 @@
+package edu.udayton.resumeeditor.util;
+
+/**
+ * Created by Rengui on 2017/8/23.
+ */
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtils {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("MM/yyyy", Locale.getDefault());
+
+    public static String dateToString(Date date) {
+        return sdf.format(date);
+    }
+
+    public static Date stringToDate(String dateString) {
+        try {
+            return sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return new Date(0);
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/udayton/resumeeditor/util/ImageUtils.java
@@ -0,0 +1,29 @@
+package edu.udayton.resumeeditor.util;
+
+/**
+ * Created by Rengui on 2017/8/23.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class ImageUtils {
+
+    public static void loadImage(@NonNull Context context,
+                                 @NonNull Uri uri,
+                                 @NonNull ImageView imageView) {
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
